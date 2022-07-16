79f1f6b78cf054dcf241ef96d600b7486320d38a
fptcmrhfptcmrh <danieljung217@gmail.com>
2022-07-16 18:49:42 +0900

    밝기에 따라 불켜기

--- light_sensor___CDS__/light_sensor___CDS__.ino
+++ light_sensor___CDS__/light_sensor___CDS__.ino
@@ -1,7 +1,16 @@
 void setup() {
-
+  Serial.begin(9600);
+  pinMode(7,OUTPUT);
 }
-
+w
 void loop() {
-
+  int lightValue = analogRead(A0);
+  Serial.println(lightValue);
+  if (lightValue<300){
+    digitalWrite(7,HIGH);
+  }
+  else{
+    digitalWrite(8,LOW);
+  }
+  delay(500);
 }
