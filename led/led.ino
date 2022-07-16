56f33ee3b0fce837b9c9f60128f7bad47427961b
fptcmrhfptcmrh <danieljung217@gmail.com>
2022-07-16 17:47:25 +0900

    led 3개 키기

--- led/led.ino
+++ led/led.ino
@@ -1,9 +1,19 @@
 void setup() {
+  pinMode(2,OUTPUT);
   pinMode(7,OUTPUT);
+  pinMode(8,OUTPUT);
 }
 void loop() {
+  digitalWrite(2,HIGH);
+  delay(100);
   digitalWrite(7,HIGH);
-  delay(500);
+  delay(100);
+  digitalWrite(8,HIGH);
+  delay(500); 
+  digitalWrite(2,LOW);
+  delay(100);
   digitalWrite(7,LOW);
+  delay(100);
+  digitalWrite(8,LOW);
   delay(500);
 }
