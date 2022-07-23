e54b2a01ebac055097a191832c31d46db62eb83c
fptcmrhfptcmrh <danieljung217@gmail.com>
2022-07-23 17:41:52 +0900

    버튼 2개로 led 2개 각각 껐다 키기

--- button/button.ino
+++ button/button.ino
@@ -1,9 +1,28 @@
 void setup() {
-  
+  Serial.begin(9600);
+  pinMode(12,INPUT);
+  pinMode(13,OUTPUT);
 
+  pinMode(7,INPUT);
+  pinMode(8,OUTPUT);
 }
 
 void loop() {
-  
-
+  int buttonValue13=digitalRead(12);
+  int buttonValue8=digitalRead(7);
+  Serial.println(buttonValue13);
+  Serial.println(buttonValue8);
+  delay(10);
+  if(buttonValue13==0){
+    digitalWrite(13,HIGH);
+  }
+  else{
+    digitalWrite(13,LOW);
+  }
+  if(buttonValue8==0){
+    digitalWrite(8,HIGH);
+  }
+  else{
+    digitalWrite(8,LOW);
+  }
 }
