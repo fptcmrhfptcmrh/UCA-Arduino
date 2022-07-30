ecbda28a05b86b6ab2ba668db06ebcf5e29bb229
fptcmrhfptcmrh <danieljung217@gmail.com>
2022-07-30 16:47:15 +0900

    DHT센서 작동 및 습도에따라 LED켜기

--- DHT/DHT.ino
+++ DHT/DHT.ino
@@ -1,9 +1,25 @@
+#include "DHT.h"
+#define DHTPIN 2
+#define DHTTYPE DHT11
+DHT dht(DHTPIN,DHTTYPE);
 void setup() {
-  // put your setup code here, to run once:
-
+  dht.begin();
+  Serial.begin(9600);
+  pinMode(7,OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  float h=dht.readHumidity();
+  float t=dht.readTemperature();
+  Serial.print("Humidity : ");
+  Serial.print(h);
+  Serial.print("   Temperature : ");
+  Serial.println(t);
+  if(h>70){
+    digitalWrite(7,HIGH);
+  }
+  else{
+    digitalWrite(7,LOW);
+  }
+  delay(100);
 }
