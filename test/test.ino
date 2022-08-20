39f3973d471321362d6ce20e629aedd1ef00a3ef
fptcmrhfptcmrh <danieljung217@gmail.com>
2022-08-20 18:26:28 +0900

    Update test.ino

--- test/test.ino
+++ test/test.ino
@@ -1,17 +1,13 @@
 void setup() {
-  pinMode(3,OUTPUT);
+  Serial.begin(9600);
 }
 void loop() {
-//  digitalWrite(3,HIGH);
-//  delay(1000);
-//  digitalWrite(3,LOW);
-//  delay(1000);
-  for (int i=0; i<256;i++){
-    analogWrite(3,i);
-    delay(10);
-  }
-  for (int i=256; i>=0;i--){
-    analogWrite(3,i);
-    delay(10);
+  char ch; 
+  if(Serial.available()) {  
+    ch = Serial.read();  
+    Serial.print(ch); 
+    if(ch>='a'&&ch<='z'){
+      Serial.print("ok");
+    }
   }
 }
